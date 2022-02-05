4bf1d852f0392bc60a4d9aec4dde5718ccd8e61f
정현수 <jung660317@naver.com>
2022-02-05 14:27:52 +0900

    feat: 스테이지, 점수 컴포넌트 생성
    - Play 페이지의 스테이지를 나타내는 컴포넌트 생성
    - Play 페이지의 점수를 나타내는 컴포넌트 생성

--- src/pages/Play/index.tsx
+++ src/pages/Play/index.tsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import AnimatedNumbers from 'react-animated-numbers';
 import * as Styled from './styled';
 
 function Play(): JSX.Element {
-  return <Styled.Container>Play</Styled.Container>;
+  const [count, setCount] = useState<number>(100000);
+  const [stage, setStage] = useState<number>(1);
+  const [active, setActive] = useState<boolean>(false);
+
+  // const onClickButton = () => {
+  //   setStage(stage + 1);
+  //   setActive(true);
+  //   setTimeout(() => {
+  //     setActive(false);
+  //   }, 100);
+  // };
+
+  useEffect(() => {
+    setInterval(() => {
+      setCount(prev => prev + 1);
+    }, 1000);
+  }, []);
+
+  return (
+    <Styled.Container>
+      <Styled.StageContainer>
+        <Styled.Stage active={active}>{stage}</Styled.Stage>
+        <Styled.StageText>스테이지</Styled.StageText>
+      </Styled.StageContainer>
+      <Styled.Point>
+        <AnimatedNumbers includeComma animateToNumber={count} />
+      </Styled.Point>
+    </Styled.Container>
+  );
 }
 
 export default Play;
--- src/pages/Play/styled.ts
+++ src/pages/Play/styled.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
+interface StageProps {
+  active: boolean;
+}
+
 export const Container = styled.div`
   position: relative;
   display: flex;
@@ -8,4 +12,36 @@ export const Container = styled.div`
   align-items: center;
   width: 100vw;
   height: 100vh;
+`;
+
+export const StageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Stage = styled.span`
+  animation: ${(props: StageProps) => props.active && `scaleDown 0.4s infinite ease`};
+  font-size: 56px;
+  font-weight: bold;
+
+  @keyframes scaleDown {
+    0% {
+      transform: scale(3);
+    }
+    30% {
+      transform: scale(1.1);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+`;
+
+export const StageText = styled.span`
+  font-size: 30px;
+`;
+
+export const Point = styled.span`
+  font-size: 40px;
 `;
