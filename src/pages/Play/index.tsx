4bd06faf0d5ff21f4f4a60af175b88fd404295fa
정현수 <jung660317@naver.com>
2022-02-05 15:40:58 +0900

    chore: 임시 eslint disable 추가

--- src/pages/Play/index.tsx
+++ src/pages/Play/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState, useEffect } from 'react';
 import { Board, Timer, Stage, Point } from '../../components';
 import useTimer from '../../hooks/useTimer';
