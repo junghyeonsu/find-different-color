5efb97ffb9d7b6c6cb1bba0563615e764e433ad4
정현수 <jung660317@naver.com>
2022-02-05 14:35:58 +0900

    feat: Board 컴포넌트 생성
    - 색깔을 띄울 Board 컴포넌트 생성

---
+++ src/components/Board/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as Styled from './styled';
+
+function Board(): JSX.Element {
+  return <Styled.Container>Board</Styled.Container>;
+}
+
+export default Board;
---
+++ src/components/Board/styled.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 360px;
+  height: 360px;
+  background-color: gray;
+`;
