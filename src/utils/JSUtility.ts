545c0739b9dedb25d68ec8db34bd40269a7dae0e
정현수 <jung660317@naver.com>
2022-02-12 12:46:44 +0900

    refactor: includeCommaInNumber 함수 간략화

--- src/utils/JSUtility.ts
+++ src/utils/JSUtility.ts
@@ -1,11 +1,10 @@
 export default {
-  includeCommaInNumber: (number: number): string => {
-    return number
+  includeCommaInNumber: (number: number): string =>
+    number
       .toString()
       .split('')
       .reverse()
       .map((v, i) => (i % 3 === 0 && i !== 0 ? `${v},` : v))
       .reverse()
-      .join('');
-  },
+      .join(''),
 };
