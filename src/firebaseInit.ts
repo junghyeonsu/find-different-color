2cc836477457e93ebcbf8b79703a953f8c7e2c7e
정현수 <jung660317@naver.com>
2022-02-13 10:03:00 +0900

    feat: 파이어베이스 초기 설정

---
+++ src/firebaseInit.ts
@@ -0,0 +1,24 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfigProd = {
+  apiKey: 'AIzaSyDejuFGuGRiJxuaWbEGCtW4q-5q607qrsU',
+  authDomain: 'find-different-color-prod.firebaseapp.com',
+  projectId: 'find-different-color-prod',
+  storageBucket: 'find-different-color-prod.appspot.com',
+  messagingSenderId: '70454444568',
+  appId: '1:70454444568:web:af2cac811620b43841b96d',
+  measurementId: 'G-V0SQGCSYCL',
+};
+
+const firebaseConfigDev = {
+  apiKey: 'AIzaSyCLRmh9ltvRkom5KhdiqdDA476OQJD3jXQ',
+  authDomain: 'find-different-color-dev-a4990.firebaseapp.com',
+  projectId: 'find-different-color-dev-a4990',
+  storageBucket: 'find-different-color-dev-a4990.appspot.com',
+  messagingSenderId: '873766327160',
+  appId: '1:873766327160:web:4d26bb65de18f8ba50ae8d',
+};
+
+const config = process.env.NODE_ENV === 'development' ? firebaseConfigDev : firebaseConfigProd;
+
+export default initializeApp(config);
