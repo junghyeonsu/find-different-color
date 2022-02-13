4fa31486bf5cdd0d4ecf03bf1822229aa20c0866
정현수 <jung660317@naver.com>
2022-02-13 21:27:31 +0900

    refactor: collection, userName 상수화

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,9 @@ export const INITIAL_TIME = 15;
 export const ONE_SECOND = 1000; // ms
 
 export const BOARD_SIZE = 360;
+
+// firestore
+export const USERS_COLLECTION = 'users';
+
+// field
+export const USER_NAME = 'userName';
