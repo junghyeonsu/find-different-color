6eed06f883376dbfa5d1c243b542343eb1c7746b
정현수 <jung660317@naver.com>
2022-02-14 16:14:00 +0900

    feat: 랭킹 페이지 디자인 완료

--- src/firebaseInit.ts
+++ src/firebaseInit.ts
@@ -2,26 +2,37 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfigProd = {
-  apiKey: 'AIzaSyDejuFGuGRiJxuaWbEGCtW4q-5q607qrsU',
-  authDomain: 'find-different-color-prod.firebaseapp.com',
-  projectId: 'find-different-color-prod',
-  storageBucket: 'find-different-color-prod.appspot.com',
-  messagingSenderId: '70454444568',
-  appId: '1:70454444568:web:af2cac811620b43841b96d',
-  measurementId: 'G-V0SQGCSYCL',
+  apiKey: process.env.REACT_APP_PROD_API_KEY,
+  authDomain: process.env.REACT_APP_PROD_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROD_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_PROD_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_PROD_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_PROD_APP_ID,
+  measurementId: process.env.REACT_APP_PROD_MEASUREMENT_ID,
 };
 
 const firebaseConfigDev = {
-  apiKey: 'AIzaSyCLRmh9ltvRkom5KhdiqdDA476OQJD3jXQ',
-  authDomain: 'find-different-color-dev-a4990.firebaseapp.com',
-  projectId: 'find-different-color-dev-a4990',
-  storageBucket: 'find-different-color-dev-a4990.appspot.com',
-  messagingSenderId: '873766327160',
-  appId: '1:873766327160:web:4d26bb65de18f8ba50ae8d',
+  apiKey: process.env.REACT_APP_DEV_API_KEY,
+  authDomain: process.env.REACT_APP_DEV_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_DEV_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_DEV_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_DEV_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_DEV_APP_ID,
 };
 
-const config = process.env.NODE_ENV !== 'development' ? firebaseConfigDev : firebaseConfigProd;
+const firebaseConfigTemp = {
+  apiKey: process.env.REACT_APP_TEMP_API_KEY,
+  authDomain: process.env.REACT_APP_TEMP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_TEMP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_TEMP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_TEMP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_TEMP_APP_ID,
+};
 
+// const config = process.env.NODE_ENV === 'development' ? firebaseConfigDev : firebaseConfigProd;
+const config = firebaseConfigTemp;
+
+// initializeApp(config);
 initializeApp(config);
 const firestore = getFirestore();
 
--- src/pages/Rank/index.tsx
+++ src/pages/Rank/index.tsx
@@ -12,31 +12,82 @@ function Rank(): JSX.Element {
   const [userList, setUserList] = useState<UsersRecordProps[] | DocumentData[]>([]);
 
   useEffect(() => {
+    console.log('실행');
+
     (async () => {
-      const records = await getRecordsInStore();
-      const recordsMap = records.docs.map(doc => doc.data());
-      setUserList(recordsMap);
+      setUserList(await getRecordsInStore());
     })();
-  }, [getRecordsInStore]);
+  }, []);
 
   return (
     <Styled.Container>
-      <Link to="/">
-        <Styled.HomeButton>홈으로</Styled.HomeButton>
-      </Link>
-      <Styled.Title>랭킹</Styled.Title>
-      <Styled.RankContainer>
-        {userList.map((data, index) => {
-          return (
-            <Styled.Row key={data.time}>
-              <Styled.Rank>{index + 1}등</Styled.Rank>
-              <Styled.UserName>{data.userName}님</Styled.UserName>
-              <Styled.Stage>{data.stage}단계</Styled.Stage>
-              <Styled.Point>{JSUtility.includeCommaInNumber(data.point)}점</Styled.Point>
-            </Styled.Row>
-          );
-        })}
-      </Styled.RankContainer>
+      <Styled.Header>
+        <Link to="/">
+          <Styled.HomeButton src="/leftArrow.png" alt="go home button" />
+        </Link>
+        <Styled.TitleContaienr>
+          <Styled.Title>랭킹</Styled.Title>
+          <Styled.SubTitle>랭킹은 1위부터 100위까지만 표시됩니다.</Styled.SubTitle>
+        </Styled.TitleContaienr>
+      </Styled.Header>
+      <Styled.Table>
+        <Styled.THead>
+          <Styled.THeadRow>
+            <Styled.Rank>랭킹</Styled.Rank>
+            <Styled.UserName>이름</Styled.UserName>
+            <Styled.Stage>단계</Styled.Stage>
+            <Styled.Point>점수</Styled.Point>
+          </Styled.THeadRow>
+        </Styled.THead>
+        <Styled.TBody>
+          {userList.map((data, index) => {
+            if (index + 1 === 1) {
+              return (
+                <Styled.TBodyRow key={data.time}>
+                  <Styled.Rank>🥇 {index + 1}등</Styled.Rank>
+                  <Styled.RankerUserName>{data.userName}</Styled.RankerUserName>
+                  <Styled.Stage>{data.stage}</Styled.Stage>
+                  <Styled.RankerPoint>
+                    {JSUtility.includeCommaInNumber(data.point)}
+                  </Styled.RankerPoint>
+                </Styled.TBodyRow>
+              );
+            }
+            if (index + 1 === 2) {
+              return (
+                <Styled.TBodyRow key={data.time}>
+                  <Styled.Rank>🥈 {index + 1}등</Styled.Rank>
+                  <Styled.RankerUserName>{data.userName}</Styled.RankerUserName>
+                  <Styled.Stage>{data.stage}</Styled.Stage>
+                  <Styled.RankerPoint>
+                    {JSUtility.includeCommaInNumber(data.point)}
+                  </Styled.RankerPoint>
+                </Styled.TBodyRow>
+              );
+            }
+            if (index + 1 === 3) {
+              return (
+                <Styled.TBodyRow key={data.time}>
+                  <Styled.Rank>🥉 {index + 1}등</Styled.Rank>
+                  <Styled.RankerUserName>{data.userName}</Styled.RankerUserName>
+                  <Styled.Stage>{data.stage}</Styled.Stage>
+                  <Styled.RankerPoint>
+                    {JSUtility.includeCommaInNumber(data.point)}
+                  </Styled.RankerPoint>
+                </Styled.TBodyRow>
+              );
+            }
+            return (
+              <Styled.TBodyRow key={data.time}>
+                <Styled.Rank>{index + 1}등</Styled.Rank>
+                <Styled.UserName>{data.userName}</Styled.UserName>
+                <Styled.Stage>{data.stage}</Styled.Stage>
+                <Styled.Point>{JSUtility.includeCommaInNumber(data.point)}</Styled.Point>
+              </Styled.TBodyRow>
+            );
+          })}
+        </Styled.TBody>
+      </Styled.Table>
     </Styled.Container>
   );
 }
